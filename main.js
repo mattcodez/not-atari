8fac4de5c59b9c3a1b8c90013b9f63d69de90c1d
Matt Molnar <matthew.molnar@gmail.com>
2016-07-24 14:37:56 -0400

    Sort of drawing a square

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 /* globals */
-const renderBuffer = new ArrayBuffer(640 * 480 * 4);
-let renderData = new Uint8ClampedArray(renderBuffer);
-//renderData.fill(200); //Temp gray fill
+const screenW = 640,
+      screenH = 480,
+      renderBuffer = new ArrayBuffer(screenW * screenH * 4),
+      renderData = new Uint8ClampedArray(renderBuffer),
+      originPixel = renderData.length - 1 - (screenW * 4);
+renderData.fill(200); //Temp gray fill
 
 function init(){
   const canvas = document.getElementById('viewer');
@@ -12,13 +15,31 @@ function init(){
   canvasDataHash.data.set(renderData);
   context.putImageData(canvasDataHash, 0, 0);
 
-  let i = 0;//pulse gray just to get some change going
-  const colorShift = function(){
-    i = ++i > 255 ? 0 : i;
-    renderData.fill(i);
+  window.setInterval(gameTick, 10); //100 updates per second
+
+  (function drawData(){
     canvasDataHash.data.set(renderData);
     context.putImageData(canvasDataHash, 0, 0);
-    window.requestAnimationFrame(colorShift);
-  };
-  window.requestAnimationFrame(colorShift);
+    window.requestAnimationFrame(drawData);
+  })();
+}
+
+function gameTick(){
+  drawCharacter();
+}
+
+function drawCharacter(){
+  //square
+  const //position and size
+    x = 20,
+    y = 0,
+    size = 30;
+
+  //draw the square a whole horizontal line at a time
+  const upALine = screenW * 4;
+  for (let i = 0; i < size; i++){
+    const lineStart =
+      (originPixel + (x * 4)) - (upALine * (size - i));
+    renderData.fill(0, lineStart, lineStart + (size * 4));
+  }
 }
